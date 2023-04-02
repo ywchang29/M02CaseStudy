779d69b22ac1519ac3c5f87dc3616fe4e3db4128
YuWei Chang <113270725+ywchang29@users.noreply.github.com>
2023-04-02 19:24:42 -0400

    Add files via upload

---
+++ M02CaseStudy.py
@@ -0,0 +1,17 @@
+#Name:Yu-Wei Chang
+#File name: M02CaseStudy
+#Purpose: Apply the knowledge and skill in coding if...else and while statements in Python
+
+while True:
+    last_name = input("Enter student's last name (enter 'end' to quit): ")
+    if last_name == 'end':
+        break
+
+    first_name = input("Enter student's first name: ")
+    gpa = float(input("Enter student's GPA: "))
+
+    if gpa >= 3.5:
+        print(f"{first_name} {last_name} has made the Dean's List!")
+    elif gpa >= 3.25:
+        print(f"{first_name} {last_name} has made the Honor Roll.")
+
